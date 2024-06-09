aa9db76859b2509de118229f97e3177b91e90e99
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-09 23:18:15 +0530

    form update,dynamic done

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const AddStudent = () => {
+    const [data,getData]= useState(
+        {
+            "firstname":"",
+            "lastname":"",
+            "clg":"",
+            "course":"",
+            "mobile":"",
+            "email":"",
+            "dob":"",
+            "address":""        }
+    )
+    const inputHandler=(event)=>{
+        getData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <NavBar/>
@@ -11,38 +28,38 @@ const AddStudent = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label htmlFor="" className="form-label">First Name</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" name='firstname' value={data.firstname} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">Last Name</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='lastname' value={data.lastname} onChange={inputHandler} />
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">College</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='clg' value={data.clg} onChange={inputHandler} />
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">DOB</label>
-                <input type="date" name="" id="" className="form-control" />
+                <input type="date" name="dob" id="" className="form-control" value={data.dob} onChange={inputHandler} />
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">Course</label>
-                <textarea name="" id="" className="form-control"></textarea>
+                <textarea name="course" id="" className="form-control" value={data.course} onChange={inputHandler} ></textarea>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">Mobile No</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" name='mobile' value={data.mobile} onChange={inputHandler} />
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">Email</label>
-                <input type="email" name="" id="" className="form-control" />
+                <input type="email" name="email" id="" className="form-control" value={data.email} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                 <label htmlFor="" className="form-label">Address</label>
-                <textarea name="" id="" className="form-control"></textarea>
+                <textarea name="address" id="" className="form-control" value={data.address} onChange={inputHandler}></textarea>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-success">Submit</button>
+                    <button onClick={readValue} className="btn btn-success">Submit</button>
                 </div>
             </div>
 
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "./NavBar";
+import axios from "axios";
 
 const ViewAll = () => {
+    const [student,getStudent] = useState([])
+    const fetchData=()=>{
+        axios.get('https://anishpdm.github.io/dummy-api-new/student.json').then(
+            (response)=>{
+                getStudent(response.data)
+                console.log(response.data)
+            }
+        ).catch().finally()
+    }
+useEffect(()=>{fetchData()},[])
+
   return (
     <div>
       <NavBar />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div className="row g-3">
-              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            
                 <table class="table">
                   <thead>
                     <tr>
@@ -21,17 +32,23 @@ const ViewAll = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                      <td>@mdo</td>
-                    </tr>
+                    {
+                        student.map(
+                            (value,index)=>{
+                                return  <tr>
+                                <th scope="row">{value._id}</th>
+                                <td>{value.address}</td>
+                                <td>{value.college}</td>
+                                <td>{value.dob}</td>
+                                <td>{value.email}</td>
+                              </tr>
+                            }
+                        )
+                    }
+                   
                   </tbody>
                 </table>
-              </div>
-            </div>
+             
           </div>
         </div>
       </div>
