ce4dc17c5430f5725d2ab6be4d7399720a959ec8
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-09 22:42:58 +0530

    viewall,navbar,routing done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import AddStudent from './components/AddStudent';
 import SearchStudent from './components/SearchStudent';
+import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      
-    <AddStudent/>
-    <SearchStudent/>
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddStudent/>}/>
+      <Route path='/search' element={<SearchStudent/>}/>
+      <Route path='/view' element={<ViewAll/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const ViewAll = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Id</th>
+                      <th scope="col">Name</th>
+                      <th scope="col">Course</th>
+                      <th scope="col">DOB</th>
+                      <th scope="col">College</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewAll;
