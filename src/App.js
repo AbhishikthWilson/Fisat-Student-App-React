ca9a324b33d2fd801f166882f04c74b78eada126
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-09 22:23:19 +0530

    search complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AddStudent from './components/AddStudent';
+import SearchStudent from './components/SearchStudent';
 
 function App() {
   return (
     <div >
       
     <AddStudent/>
+    <SearchStudent/>
 
     </div>
   );
